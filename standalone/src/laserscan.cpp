4ecb88633678c8dff1cede8e8f2fd2ec35f89854
Immanuel Martini <i.martini@pilz.de>
2021-09-16 15:45:01 +0200

    Rebase from 'debian/noetic/psen_scan_v2'

--- standalone/src/laserscan.cpp
+++ standalone/src/laserscan.cpp
@@ -14,10 +14,14 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <algorithm>
+#include <ostream>
 #include <stdexcept>
+
+#include <fmt/format.h>
 
 #include "psen_scan_v2_standalone/data_conversion_layer/angle_conversions.h"
 #include "psen_scan_v2_standalone/laserscan.h"
+#include "psen_scan_v2_standalone/util/format_range.h"
 
 namespace psen_scan_v2_standalone
 {
@@ -25,8 +29,14 @@ static const util::TenthOfDegree MAX_X_AXIS_ROTATION{ 275 };
 
 LaserScan::LaserScan(const util::TenthOfDegree& resolution,
                      const util::TenthOfDegree& min_scan_angle,
-                     const util::TenthOfDegree& max_scan_angle)
-  : resolution_(resolution), min_scan_angle_(min_scan_angle), max_scan_angle_(max_scan_angle)
+                     const util::TenthOfDegree& max_scan_angle,
+                     const uint32_t scan_counter,
+                     const int64_t timestamp)
+  : resolution_(resolution)
+  , min_scan_angle_(min_scan_angle)
+  , max_scan_angle_(max_scan_angle)
+  , scan_counter_(scan_counter)
+  , timestamp_(timestamp)
 {
   if (getScanResolution() == util::TenthOfDegree(0))
   {
@@ -64,6 +74,16 @@ const LaserScan::MeasurementData& LaserScan::getMeasurements() const
   return measurements_;
 }
 
+uint32_t LaserScan::getScanCounter() const
+{
+  return scan_counter_;
+}
+
+int64_t LaserScan::getTimestamp() const
+{
+  return timestamp_;
+}
+
 void LaserScan::setMeasurements(const MeasurementData& measurements)
 {
   measurements_ = measurements;
@@ -84,11 +104,18 @@ void LaserScan::setIntensities(const IntensityData& intensities)
   intensities_ = intensities;
 }
 
-bool LaserScan::operator==(const LaserScan& scan) const
+std::ostream& operator<<(std::ostream& os, const LaserScan& scan)
 {
-  return ((max_scan_angle_ == scan.max_scan_angle_) && (min_scan_angle_ == scan.min_scan_angle_) &&
-          (resolution_ == scan.resolution_) && (measurements_.size() == scan.measurements_.size()) &&
-          std::equal(measurements_.begin(), measurements_.end(), scan.measurements_.begin()));
+  os << fmt::format("LaserScan(timestamp = {} nsec, scanCounter = {}, minScanAngle = {} deg, maxScanAngle = {} deg, "
+                    "resolution = {} deg, measurements = {}, intensities = {})",
+                    scan.getTimestamp(),
+                    scan.getScanCounter(),
+                    scan.getMinScanAngle().value() / 10.,
+                    scan.getMaxScanAngle().value() / 10.,
+                    scan.getScanResolution().value() / 10.,
+                    util::formatRange(scan.getMeasurements()),
+                    util::formatRange(scan.getIntensities()));
+  return os;
 }
 
 }  // namespace psen_scan_v2_standalone
