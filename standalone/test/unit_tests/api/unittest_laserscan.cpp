9749923deee24f82ca34552d766b33477d4a6b05
Immanuel Martini <i.martini@pilz.de>
2021-09-16 15:44:50 +0200

    Rebase from 'upstream'

--- standalone/test/unit_tests/api/unittest_laserscan.cpp
+++ standalone/test/unit_tests/api/unittest_laserscan.cpp
@@ -18,6 +18,9 @@
 
 #include <gtest/gtest.h>
 
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
 #include "psen_scan_v2_standalone/laserscan.h"
 
 using namespace psen_scan_v2_standalone;
@@ -27,54 +30,142 @@ namespace psen_scan_v2_standalone_test
 static const util::TenthOfDegree DEFAULT_RESOLUTION{ 1 };
 static const util::TenthOfDegree DEFAULT_START_ANGLE{ 1 };
 static const util::TenthOfDegree DEFAULT_END_ANGLE{ 2 };
+static const int64_t DEFAULT_TIMESTAMP{ 1 };
+static const uint32_t DEFAULT_SCAN_COUNTER{ 1 };
+
+class LaserScanBuilder
+{
+public:
+  LaserScan build()
+  {
+    return LaserScan(resolution_, start_angle_, end_angle_, scan_counter_, timestamp_);
+  }
+
+  LaserScanBuilder& resolution(const util::TenthOfDegree& resolution)
+  {
+    resolution_ = resolution;
+    return *this;
+  }
+
+  LaserScanBuilder& startAngle(const util::TenthOfDegree& start_angle)
+  {
+    start_angle_ = start_angle;
+    return *this;
+  }
+
+  LaserScanBuilder& endAngle(const util::TenthOfDegree& end_angle)
+  {
+    end_angle_ = end_angle;
+    return *this;
+  }
+
+  LaserScanBuilder& scanCounter(const uint32_t scan_counter)
+  {
+    scan_counter_ = scan_counter;
+    return *this;
+  }
+
+  LaserScanBuilder& timestamp(const int64_t timestamp)
+  {
+    timestamp_ = timestamp;
+    return *this;
+  }
+
+private:
+  util::TenthOfDegree resolution_{ DEFAULT_RESOLUTION };
+  util::TenthOfDegree start_angle_{ DEFAULT_START_ANGLE };
+  util::TenthOfDegree end_angle_{ DEFAULT_END_ANGLE };
+  uint32_t scan_counter_{ DEFAULT_SCAN_COUNTER };
+  int64_t timestamp_{ DEFAULT_TIMESTAMP };
+};
 
 TEST(LaserScanTests, testInvalidZeroResolution)
 {
-  EXPECT_THROW(LaserScan laser_scan(util::TenthOfDegree(0), DEFAULT_START_ANGLE, DEFAULT_END_ANGLE),
-               std::invalid_argument);
+  LaserScanBuilder laser_scan_builder;
+  laser_scan_builder.resolution(util::TenthOfDegree(0));
+  EXPECT_THROW(laser_scan_builder.build(), std::invalid_argument);
 }
 
 TEST(LaserScanTests, testOutOfRangeResolution)
 {
-  EXPECT_THROW(LaserScan laser_scan(util::TenthOfDegree(10000), DEFAULT_START_ANGLE, DEFAULT_END_ANGLE),
-               std::invalid_argument);
+  LaserScanBuilder laser_scan_builder;
+  laser_scan_builder.resolution(util::TenthOfDegree(10000));
+  EXPECT_THROW(laser_scan_builder.build(), std::invalid_argument);
 }
 
 TEST(LaserScanTests, testInvalidStartEndAngle)
 {
-  EXPECT_THROW(LaserScan laser_scan(DEFAULT_RESOLUTION, DEFAULT_END_ANGLE, DEFAULT_START_ANGLE), std::invalid_argument);
+  LaserScanBuilder laser_scan_builder;
+  laser_scan_builder.startAngle(DEFAULT_END_ANGLE).endAngle(DEFAULT_START_ANGLE);
+  EXPECT_THROW(laser_scan_builder.build(), std::invalid_argument);
 }
 
 TEST(LaserScanTest, testGetScanResolution)
 {
-  const util::TenthOfDegree expected_resolution{ DEFAULT_RESOLUTION };
+  LaserScanBuilder laser_scan_builder;
   std::unique_ptr<LaserScan> laser_scan;
-  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(expected_resolution, DEFAULT_START_ANGLE, DEFAULT_END_ANGLE)););
-
-  EXPECT_EQ(expected_resolution, laser_scan->getScanResolution());
+  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(laser_scan_builder.build())););
+  EXPECT_EQ(DEFAULT_RESOLUTION, laser_scan->getScanResolution());
 }
 
 TEST(LaserScanTest, testGetMinScanAngle)
 {
-  const auto expected_min_scan_angle{ DEFAULT_START_ANGLE };
+  LaserScanBuilder laser_scan_builder;
   std::unique_ptr<LaserScan> laser_scan;
-  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(DEFAULT_RESOLUTION, expected_min_scan_angle, DEFAULT_END_ANGLE)););
-
-  EXPECT_EQ(expected_min_scan_angle, laser_scan->getMinScanAngle());
+  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(laser_scan_builder.build())););
+  EXPECT_EQ(DEFAULT_START_ANGLE, laser_scan->getMinScanAngle());
 }
 
 TEST(LaserScanTest, testGetMaxScanAngle)
 {
-  const auto expected_max_scan_angle{ DEFAULT_END_ANGLE };
+  LaserScanBuilder laser_scan_builder;
   std::unique_ptr<LaserScan> laser_scan;
-  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(DEFAULT_RESOLUTION, DEFAULT_START_ANGLE, expected_max_scan_angle)););
-
-  EXPECT_EQ(expected_max_scan_angle, laser_scan->getMaxScanAngle());
+  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(laser_scan_builder.build())););
+  EXPECT_EQ(DEFAULT_END_ANGLE, laser_scan->getMaxScanAngle());
 }
 
 TEST(LaserScanTest, testMinEqualsMax)
 {
-  ASSERT_NO_THROW(LaserScan(DEFAULT_RESOLUTION, util::TenthOfDegree(1000u), util::TenthOfDegree(1000u)););
+  LaserScanBuilder laser_scan_builder;
+  laser_scan_builder.startAngle(util::TenthOfDegree(1000u)).endAngle(util::TenthOfDegree(1000u));
+  ASSERT_NO_THROW(laser_scan_builder.build(););
+}
+
+TEST(LaserScanTest, testGetScanCounter)
+{
+  LaserScanBuilder laser_scan_builder;
+  std::unique_ptr<LaserScan> laser_scan;
+  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(laser_scan_builder.build())););
+  EXPECT_EQ(DEFAULT_SCAN_COUNTER, laser_scan->getScanCounter());
+}
+
+TEST(LaserScanTest, testGetTimestamp)
+{
+  LaserScanBuilder laser_scan_builder;
+  std::unique_ptr<LaserScan> laser_scan;
+  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(laser_scan_builder.build())););
+  EXPECT_EQ(DEFAULT_TIMESTAMP, laser_scan->getTimestamp());
+}
+
+TEST(LaserScanTest, testPrintMessageSuccess)
+{
+  LaserScanBuilder laser_scan_builder;
+  std::unique_ptr<LaserScan> laser_scan;
+  ASSERT_NO_THROW(laser_scan.reset(new LaserScan(laser_scan_builder.build())););
+
+  laser_scan->setMeasurements({ 45.0, 44.0, 43.0, 42.0 });
+
+// For compatibility with different ubuntu versions (resp. fmt), we need to take account of changes in
+// the default formatting of floating point numbers
+#if (FMT_VERSION >= 60000 && FMT_VERSION < 70100)
+  EXPECT_EQ(fmt::format("{}", *laser_scan),
+            "LaserScan(timestamp = 1 nsec, scanCounter = 1, minScanAngle = 0.1 deg, maxScanAngle = 0.2 deg, resolution "
+            "= 0.1 deg, measurements = {45.0, 44.0, 43.0, 42.0}, intensities = {})");
+#else
+  EXPECT_EQ(fmt::format("{}", *laser_scan),
+            "LaserScan(timestamp = 1 nsec, scanCounter = 1, minScanAngle = 0.1 deg, maxScanAngle = 0.2 deg, resolution "
+            "= 0.1 deg, measurements = {45, 44, 43, 42}, intensities = {})");
+#endif
 }
 
 }  // namespace psen_scan_v2_standalone_test
